5e511969350470ce0ceef26375545144b3c81940
will121173 <6d6173616b69@gmail.com>
2023-09-07 23:00:04 +0000

    readme

--- src/web/sampleJS.js
+++ src/web/sampleJS.js
@@ -2,7 +2,7 @@
 // {"message":"Hello World"}のmessage=Hello Worldに書き換え
 
 // ↓↓portsタブ・port3000の行のAdressを記入
-const apiUrl = 'https://3000-will121173-apipracticep-m4y3pyvx931.ws-us104.gitpod.io';
+const apiUrl = 'https://～～～～～～～～～～～～～～～～.gitpod.io';
 const element = document.getElementById("content1");
 fetch(apiUrl)
 .then((res) => res.json())
